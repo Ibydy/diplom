2a9655ccbf0bd9d701e234a275a75c19686675bc
lev <ibydyl1@gmail.com>
2017-04-16 17:17:34 +0300

    description Dynamic visual acuity created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,25 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from "@angular/router";
+import { PreviewComponent } from './components/preview/preview.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NofoundComponent } from './components/nofound/nofound.component';
+
+const appRoutes: Routes = [
+  { path: 'preview', component: PreviewComponent },
+  { path: '**', component: NofoundComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PreviewComponent,
+    HeaderComponent,
+    NofoundComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule
